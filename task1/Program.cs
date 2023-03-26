92cd126cb6f4cf86dbc6c5344382daf33d09d77c
Zeradeil <wandark@ya.ru>
2023-03-26 20:34:43 +0700

    задания выполнены

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее,
+//  а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.Write("Введите первое число => ");
+int number1 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите второе число => ");
+int number2 = Convert.ToInt32(System.Console.ReadLine());
+if (number1 > number2)
+{
+    System.Console.WriteLine($"Чило {number1} больше числа {number2}");
+}
+else
+{
+    System.Console.WriteLine($"Чило {number2} больше числа {number1}");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.Write("Введите число 1 => ");
+int number1 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число 2 => ");
+int number2 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число 3 => ");
+int number3 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine($"Максимальное число = {Math.Max(number1, Math.Max(number2, number3))}");
